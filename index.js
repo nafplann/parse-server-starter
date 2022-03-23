099709efb4ee37977c4a983738d530214f04aca1
Abdul Manaaf <nafplann@gmail.com>
2022-03-23 22:33:55 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,42 @@
 const express = require('express');
 const ParseServer = require('parse-server').ParseServer;
+const ParseDashboard = require('parse-dashboard');
 const app = express();
-const fs = require('fs');
 const port = 1337;
-const os = require( 'os' );
-const networkInterfaces = os.networkInterfaces();
-const ipAddress = networkInterfaces['eth0'][0]['address'];
+const ipAddress = '127.0.0.1';
 const dbName = 'parse';
+const appName = 'parse';
 
 const api = new ParseServer({
-    appName: 'APP_NAME',
-    databaseURI: `mongodb://localhost:27017/${dbName}`,
-    appId: 'APP_ID',
-    masterKey: 'APP_MASTER_KEY',
-    serverURL: `http://${ipAddress}:1337/${dbName}`
-    cloud: __dirname + '/cloud/main.js'
+  appName: 'APP_NAME',
+  databaseURI: `postgres://postgres:1q2w3e4r5@localhost:5432/${dbName}?ssl=false&application_name=${appName}`,
+  appId: 'APP_ID',
+  masterKey: 'APP_MASTER_KEY',
+  serverURL: `http://${ipAddress}:${port}/dbs/${dbName}`,
+  cloud: __dirname + '/cloud/main.js'
 });
 
-app.use(`/${dbName}`, api);
+const dashboard = new ParseDashboard({
+  apps: [
+    {
+      'serverURL': `http://${ipAddress}:${port}/dbs/${dbName}`,
+      'appId': 'APP_ID',
+      'masterKey': 'APP_MASTER_KEY',
+      'appName': 'APP_NAME',
+      'iconName': 'icon.jpg'
+    }
+  ],
+  users: [
+    { 'user': 'user', 'pass': 'pass' }
+  ],
+  iconsFolder: 'icons'
+}, true);
+
+app.use(`/dbs/${dbName}`, api);
+app.use(`/`, dashboard);
 
 const httpServer = require('http').createServer(app);
 
 httpServer.listen(port, () => {
-    console.log('parse-server-starter is running on port ', port);
+  console.log(`parse-server-starter is running on ${ipAddress}:${port}`);
 });
